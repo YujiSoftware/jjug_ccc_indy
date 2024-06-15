b4929680b1739b621b84886974268b8ecbd76bc2
YujiSoftware <yuji.software+github@gmail.com>
2024-06-15 23:51:44 +0900

    口頭のコメント反映

--- src/StringConcat.java
+++ src/StringConcat.java
@@ -1,13 +1,10 @@
-import java.time.LocalDateTime;
-
 public class StringConcat {
     public static void main(String[] args) {
-        LocalDateTime date = LocalDateTime.now();
-        String text = makeText(date.getHour(), date.getMinute());
+        String text = makeText(123);
         System.out.println(text);
     }
 
-    private static String makeText(int hour, int minute) {
-        return "ただいまの時刻は" + hour + "時" + minute + "分です。";
+    private static String makeText(int count) {
+        return "Total:" + count + "files";
     }
 }
